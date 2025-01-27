8dcc0b8d1422a6ea7cfd344929a5e2b7d99a5308
Anish <anishkhamkar7@gmail.com>
2025-01-27 22:11:01 +0530

    [Backend] chore: added route import in the index  file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from "express";
-
+import todoRouter from "./routes";
 const app = express();
+
+app.use(express.json());
+
+app.use("/", todoRouter);
 
 app.listen(9000, () => {
   console.log("Server Running");

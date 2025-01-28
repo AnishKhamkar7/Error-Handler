c579716f2199dea68f6bc08719cc5f61bca89ff0
Anish <anishkhamkar7@gmail.com>
2025-01-28 21:46:30 +0530

    [Backend] feat: added asyncHandler from ErrorHandler class to routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import TodoController from "./todo";
 import { Router } from "express";
+import ErrorManager from "./ErrorManager";
 
 const router = Router();
 
 const todoController = new TodoController();
-router.route("/api/todos").get(todoController.getTodo);
+router.route("/api/todos").get(ErrorManager.handle(todoController.getTodo));
 
 export default router;

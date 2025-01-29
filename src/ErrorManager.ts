75ad11dcd691af309844fde0ed320a41d64e150d
Anish <anishkhamkar7@gmail.com>
2025-01-29 11:23:42 +0530

    [Backend] feta: added Winston boiler-code in ErrorManager

--- src/ErrorManager.ts
+++ src/ErrorManager.ts
@@ -1,4 +1,26 @@
+import { error } from "console";
 import { Request, Response, NextFunction } from "express";
+import { createLogger, format, transports } from "winston";
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6,
+};
+
+const logger = createLogger({
+  levels,
+  format: format.json(),
+  defaultMeta: { service: "user-service" },
+  transports: [
+    new transports.File({ filename: "error.log", level: "error" }),
+    new transports.File({ filename: "combined.log" }),
+  ],
+});
 
 type AsyncFunction = (
   req: Request,
@@ -12,4 +34,6 @@ export default class ErrorManager {
       Promise.resolve(fn(req, res, next)).catch(next);
     };
   }
+
+  static;
 }
